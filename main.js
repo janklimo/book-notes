b7e8fecb823f15500d3504f5391f6c45a3576634
Jan Klimo <jan.klimo@gmail.com>
2016-11-08 22:33:31 +0700

    Data persisted in AsyncStorage

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ import Immutable from 'immutable'
 import { createStore } from 'redux'
 import { combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import {AsyncStorage} from 'react-native';
 
 import App from './components/App'
 import notesReducer from './store/notes/reducer'
 
+import {persistStore, autoRehydrate} from 'redux-persist'
+
 const reducers = combineReducers({ notes: notesReducer })
-const store = createStore(reducers)
+const store = createStore(reducers, undefined, autoRehydrate())
+persistStore(store, {storage: AsyncStorage})
 
 const Main = () => {
   return (
